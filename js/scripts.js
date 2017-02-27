2562f070c3d8df96588d2d6ba29c3bb541c947bd
Monique St Laurent <moinstla@gmail.com>
2017-02-26 16:12:09 -0800

    add photos and styling

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,36 @@ $(document).ready(function() {
    var question1 = parseInt($("#question1").val());
    var question2 = parseInt($("#question2").val());
    var question3 = parseInt($("#question3").val());
-
-
    var userScore = (question1 + question2 + question3);
 
     $("#result").show();
 
     if (userScore <= 4) {
       $("#result").text("Washington DC");
-      $("#washington").show();
-
-  } else if (userScore <= 6) {
+      $("#washingtondc").show();
+      $("#florida").hide();
+      $("#italy").hide();
+      $("#newzealand").hide();
+    } else if (userScore <= 6) {
       $("#result").text("Florida");
       $("#florida").show();
-
-  } else if (userScore <= 9) {
+      $("#italy").hide();
+      $("#washingtondc").hide();
+      $("#newzealand").hide();
+    } else if (userScore <= 9) {
       $("#result").text("Italy");
       $("#italy").show();
-
-  } else {
+      $("#florida").hide();
+      $("#washingtondc").hide();
+      $("#newzealand").hide();
+    } else {
       $("#result").text("New Zealand");
-      $("newzealand").show();
-  }
+      $("#newzealand").show();
+      $("#florida").hide();
+      $("#washingtondc").hide();
+      $("#italy").hide();
 
-
+    }
     event.preventDefault();
   });
 });
